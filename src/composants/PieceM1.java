c4600b71eb1095fea329c3b5d155daaf10484e34
Tinwynn <stanwisepala@gmail.com>
2021-04-23 23:35:22 +0200

    Step 2

---
+++ src/composants/PieceM1.java
@@ -0,0 +1,29 @@
+package composants;
+
+/**
+ *
+ * Cette classe permet de reprÃ©senter les piÃ¨ces du jeu de modÃ¨le 1.
+ *
+ */
+public class PieceM1 extends Piece {
+
+    /**
+     * A Faire (23/04/21 LG Finalisée)
+     *
+     * Constructeur permettant de construire une piÃ¨ce de modÃ¨le 1 et d'orientation 0.
+     */
+    public PieceM1() {
+        super(1,true,false,true,false);
+    }
+    /**
+     * A Faire (Quand Qui Statut)
+     *
+     * MÃ©thode permettant de crÃ©er une copie de la piÃ¨ce (un nouvelle objet Java).
+     * @return Une copie de la piÃ¨ce.
+     */
+    public Piece copy(){
+        Piece piece=null;
+        // A ComplÃ©ter
+        return piece;
+    }
+}
